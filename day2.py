de89ca2f4c7c03e5e3af64d536eb03554c1a4068
Gru Singh <gru.singh@spothero.com>
2023-12-03 08:06:43 -0500

    Day 2 Part 2 done

--- day2.py
+++ day2.py
@@ -30,7 +30,12 @@ def parse_game_from_string(val):
             green = int(color[:g_index]) if g_index > -1 else green
             blue = int(color[:b_index]) if b_index > -1 else blue
         rolls.append([red, green, blue])
-    return (id, rolls, val)
+    max_r = max([r[0] for r in rolls])
+    max_g = max([r[1] for r in rolls])
+    max_b = max([r[2] for r in rolls])
+    powerset = (max_r, max_g, max_b)
+    power = max_r * max_g * max_b
+    return (id, rolls, val, power, powerset)
 
 
 def sum_possible_game_ids(strings, max_r, max_g, max_b):
@@ -56,19 +61,30 @@ def sum_possible_game_ids(strings, max_r, max_g, max_b):
     return sum(possible_games)
 
 
+def power_of_games(strings):
+    games = map(parse_game_from_string, strings)
+    total = 0
+    for game in games:
+        # print("--------------------------------------")
+        # print(game[2])
+        total += game[3]
+
+    return total
+
+
 class TestDay2(unittest.TestCase):
     def test_read_input(self):
         self.assertEqual(len(read_input()), 100)
 
     def test_parse_value(self):
         self.assertEqual(parse_game_from_string(
-            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'), (1, [[4, 0, 3], [1, 2, 6], [0, 2, 0]], 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'))
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'), (1, [[4, 0, 3], [1, 2, 6], [0, 2, 0]], 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', 48, (4, 2, 6)))
         self.assertEqual(parse_game_from_string(
             'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'), (2, [
                 [0, 2, 1],
                 [1, 3, 4],
                 [0, 1, 1]
-            ], 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'))
+            ], 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', 12, (1, 3, 4)))
         self.assertEqual(parse_game_from_string(
             'Game 31: 9 blue, 6 red, 7 green; 20 red, 1 green, 15 blue; 6 blue, 7 green, 17 red; 2 blue, 3 green, 6 red; 1 red, 3 blue, 2 green; 5 green, 18 red, 6 blue'), (31, [
                 [6, 7, 9],
@@ -77,7 +93,7 @@ class TestDay2(unittest.TestCase):
                 [6, 3, 2],
                 [1, 2, 3],
                 [18, 5, 6],
-            ], 'Game 31: 9 blue, 6 red, 7 green; 20 red, 1 green, 15 blue; 6 blue, 7 green, 17 red; 2 blue, 3 green, 6 red; 1 red, 3 blue, 2 green; 5 green, 18 red, 6 blue'))
+            ], 'Game 31: 9 blue, 6 red, 7 green; 20 red, 1 green, 15 blue; 6 blue, 7 green, 17 red; 2 blue, 3 green, 6 red; 1 red, 3 blue, 2 green; 5 green, 18 red, 6 blue', 2100, (20, 7, 15)))
 
     def test_sum_possible_game_ids(self):
         lines = [
@@ -93,6 +109,22 @@ class TestDay2(unittest.TestCase):
         lines = read_input()
         total = sum_possible_game_ids(lines, 12, 13, 14)
         self.assertEqual(total, 2006)
+
+    def test_power_of_games(self):
+        lines = [
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+        ]
+        total = power_of_games(lines)
+        self.assertEqual(total, 2286)
+
+    def test_power_of_games_part2(self):
+        lines = read_input()
+        total = power_of_games(lines)
+        self.assertEqual(total, 84911)
 
 
 if __name__ == '__main__':
