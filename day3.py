6ed4a3ea67b99fca6c05ca6c8425df9fcdb91d1c
Gru Singh <gru.singh@spothero.com>
2023-12-06 05:48:16 -0500

    Day 3 Part 2 Done

--- day3.py
+++ day3.py
@@ -16,6 +16,19 @@ sample_values = [
     '......*001'
 ]
 
+sample_gears = [
+    '467..114..',
+    '...*......',
+    '..35..633.',
+    '......#...',
+    '617*......',
+    '.....+.58.',
+    '..592.....',
+    '......755.',
+    '...$.*....',
+    '.664.598..',
+]
+
 
 def get_symbols(ri, s, e, rows):
     row = rows[ri]
@@ -24,15 +37,12 @@ def get_symbols(ri, s, e, rows):
     cs = 0 if s == 0 else s - 1
     ce = e if e == len(row) else e + 1
 
-    # print("ri: ", ri, s, e)
-    # print("rc: ", rs, re, cs, ce)
-
     symbols = []
     for r in range(rs, re+1):
-        # print(" for: ", r, " row:", rows[r], " str:", rows[r][cs:ce])
-        for s in rows[r][cs:ce]:
+        for si in range(cs, ce):
+            s = rows[r][si]
             if not s.isdigit() and s != '.':
-                symbols.append(s)
+                symbols.append([s, r, si])
 
     return symbols
 
@@ -42,10 +52,8 @@ def find_part_numbers(lines):
 
     for li in range(len(lines)):
         line = lines[li]
-        # print(li, " => ", line)
         matches = re.finditer(f"\d+", line)
         for m in matches:
-            # print('      %d-%d: %s' % (m.start(), m.end(), m.group(0)))
             s = m.start()
             e = m.end()
             n = int(m.group(0))
@@ -57,6 +65,50 @@ def find_part_numbers(lines):
     return parts
 
 
+def find_gears(lines):
+    parts = []
+
+    for li in range(len(lines)):
+        line = lines[li]
+        matches = re.finditer(f"\d+", line)
+        for m in matches:
+            s = m.start()
+            e = m.end()
+            n = int(m.group(0))
+
+            symbols = get_symbols(li, s, e, lines)
+            if (len(symbols) > 0):
+                parts.append([n, symbols])
+
+    gears = []
+    for e in parts:
+        n = e[0]
+        sa = e[1]
+        for s in sa:
+            if s[0] == '*':
+                gear = None
+                for x in gears:
+                    if x[0] == s[1] and x[1] == s[2]:
+                        gear = x
+                        break
+
+                if gear:
+                    gear[2].append(n)
+                else:
+                    gears.append([s[1], s[2], [n]])
+
+    doubles = []
+    total = 0
+    for g in gears:
+        if len(g[2]) == 2:
+            doubles.append(g)
+            x = g[2][0]
+            y = g[2][1]
+            total += (x*y)
+
+    return total
+
+
 class TestDay3(unittest.TestCase):
     def test_read_input(self):
         self.assertEqual(len(read_input("day3.txt")), 140)
@@ -66,7 +118,14 @@ class TestDay3(unittest.TestCase):
 
     def test_find_partial_numbers_part1(self):
         values = read_input("day3.txt")
-        self.assertEqual(sum(find_part_numbers(values)), 0)
+        self.assertEqual(sum(find_part_numbers(values)), 536202)
+
+    def test_find_gears(self):
+        self.assertEqual(find_gears(sample_gears), 467835)
+
+    def test_find_gears_part2(self):
+        values = read_input("day3.txt")
+        self.assertEqual(find_gears(values), 78272573)
 
 
 if __name__ == "__main__":
