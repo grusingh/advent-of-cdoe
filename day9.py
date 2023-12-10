c4f3730afb99ad0682fa94aa68fd8cf4fcd8858f
Gru Singh <gru.singh@spothero.com>
2023-12-10 08:15:45 -0500

    Day9 Part2 Done

--- day9.py
+++ day9.py
@@ -31,14 +31,12 @@ def get_interations_to_zero(input):
             if v != 0:
                 all_zero = False
 
-        # print("r:", r)
         lines.append(r)
         line = r
 
         if all_zero:
             break
 
-    # print(lines)
     return lines
 
 
@@ -46,27 +44,34 @@ def get_next(line):
     lst = get_interations_to_zero(line)
     lst.reverse()
 
-    # print("before: ", lst)
-
     v = 0
     for l in lst:
-        # print("l", l)
-        # print("s", len(set(l)))
-
         if len(set(l)) == 1:
             v = l[-1:][0]
-            # print("v1", v)
             l.append(v)
         else:
             v = l[-1:][0] + v
-            # print("v2", v)
             l.append(v)
 
-        # print("new line", l)
+    lst.reverse()
+    return lst[0][-1:][0]
+
+
+def get_first(line):
+    lst = get_interations_to_zero(line)
+    lst.reverse()
+
+    v = 0
+    for l in lst:
+        if len(set(l)) == 1:
+            v = l[0]
+            l.insert(0, v)
+        else:
+            v = l[0] - v
+            l.insert(0, v)
 
     lst.reverse()
-    # print("after:", lst)
-    return lst[0][-1:][0]
+    return lst[0][0]
 
 
 def get_sum_part1(lines):
@@ -74,30 +79,46 @@ def get_sum_part1(lines):
     total = 0
     for d in data:
         n = get_next(d)
-        # print("n", n)
+        total += n
+    return total
+
+
+def get_sum_part2(lines):
+    data = get_data(lines)
+    total = 0
+    for d in data:
+        n = get_first(d)
         total += n
     return total
 
 
 class Test(unittest.TestCase):
-    def xtest_case(self):
+    def test_case(self):
         v = get_data(sample)
         self.assertEqual(
             v, [[0, 3, 6, 9, 12, 15], [1, 3, 6, 10, 15, 21], [10, 13, 16, 21, 30, 45]]
         )
 
-    def xtest_get_interations_to_zero(self):
+    def test_get_interations_to_zero(self):
         data = get_data(sample)
         v = get_interations_to_zero(data[0])
         self.assertEqual(v, [[0, 3, 6, 9, 12, 15], [3, 3, 3, 3, 3], [0, 0, 0, 0]])
 
-    def test_get_sum_sample(self):
+    def test_get_sum_part1_sample(self):
         total = get_sum_part1(sample)
         self.assertEqual(total, 114)
 
-    def test_get_sum_part1(self):
+    def test_get_sum_part2_sample(self):
+        total = get_sum_part2(sample)
+        self.assertEqual(total, 2)
+
+    def test_get_sum_part1_solution(self):
         total = get_sum_part1(values)
         self.assertEqual(total, 1789635132)
+
+    def test_get_sum_part2_solution(self):
+        total = get_sum_part2(values)
+        self.assertEqual(total, 913)
 
 
 if __name__ == "__main__":
