3a8cab5ea8c7d2f5e1f3253da1e9010a6b8eeb31
Gru Singh <gru.singh@spothero.com>
2023-12-12 06:21:37 -0500

    Day 11 Part2 WIP

--- day11.py
+++ day11.py
@@ -1,6 +1,6 @@
 import unittest
 import re
-from utils import read_input
+from utils import read_input, write_matrix_to_file
 
 # values = read_input("day.txt")
 sample = [
@@ -46,7 +46,7 @@ expanded_num_matrix = [
 ]
 
 
-def get_matrix(lines):
+def get_matrix(lines, filler_times=1):
     rows = []
     for r in lines:
         chars = [c for c in r]
@@ -62,17 +62,29 @@ def get_matrix(lines):
             empty_cols_idx.append(ci)
 
     # print("empty cols: ", empty_cols_idx)
-    for idx in range(len(empty_cols_idx)):
-        ci = empty_cols_idx[idx]
-        for ri in range(len(rows)):
-            rows[ri].insert(ci + idx, ".")
+    rows2 = []
+    for ri in range(len(rows)):
+        col = []
+        # print("before", rows[ri])
+        for idx in range(len(rows[ri])):
+            if len([x for x in empty_cols_idx if x == idx]) == 1:
+                # print("found filler", idx, "row ", ri)
+                for t in range(filler_times):
+                    col.append(".")
+            else:
+                col.append(rows[ri][idx])
+        # print("col len", len(col), col)
+        rows2.append(col)
 
     new_rows = []
-    for r in rows:
+    for ri in range(len(rows2)):
+        r = rows2[ri]
         new_rows.append(r)
-        if len(set(r)) == 1 and r[0] == ".":
-            # print("found empty row", len(rows))
-            new_rows.append(r)
+        s = set(r)
+        if len(s) == 1 and "." in s:
+            # print("found empty row", ri)
+            for f in range(filler_times - 1):
+                new_rows.append(r)
 
     return new_rows
 
@@ -108,8 +120,6 @@ def find_pairs(matrix):
 
 
 def get_index(matrix, v):
-    x = None
-    y = None
     for ri in range(len(matrix)):
         r = matrix[ri]
         for ci in range(len(r)):
@@ -126,6 +136,7 @@ def get_path_length(matrix, a, b):
 
 def compute_sum(matrix):
     pairs = find_pairs(matrix)
+    # print("pairs", len(pairs), pairs)
     total = 0
     for p in pairs:
         total += get_path_length(matrix, p[0], p[1])
@@ -134,14 +145,13 @@ def compute_sum(matrix):
 
 class Test(unittest.TestCase):
     def test_get_matrix(self):
-        matrix = get_matrix(sample)
+        matrix = get_matrix(sample, 2)
         expected = ["@".join(["".join(r) for r in expanded_sample])]
         result = ["@".join(["".join(r) for r in matrix])]
-
         self.assertEqual(result, expected)
 
     def test_replace_galaxies_with_numbers(self):
-        matrix = get_matrix(sample)
+        matrix = get_matrix(sample, 2)
         num_matrix = replace_galaxies_with_numbers(matrix)
         expected = ["@".join(["".join(r) for r in expanded_num_matrix])]
         result = ["@".join(["".join(r) for r in num_matrix])]
@@ -149,12 +159,12 @@ class Test(unittest.TestCase):
         self.assertEqual(result, expected)
 
     def test_pairs(self):
-        matrix = get_matrix(sample)
+        matrix = get_matrix(sample, 2)
         pairs = find_pairs(replace_galaxies_with_numbers(matrix))
         self.assertEqual(len(pairs), 36)
 
     def test_path_length(self):
-        matrix = get_matrix(sample)
+        matrix = get_matrix(sample, 2)
         num_matrix = replace_galaxies_with_numbers(matrix)
         self.assertEqual(get_path_length(num_matrix, "9", "5"), 9)
         self.assertEqual(get_path_length(num_matrix, "1", "7"), 15)
@@ -162,13 +172,31 @@ class Test(unittest.TestCase):
         self.assertEqual(get_path_length(num_matrix, "8", "9"), 5)
 
     def test_sample_sum(self):
-        matrix = get_matrix(sample)
+        matrix = get_matrix(sample, 2)
         num_matrix = replace_galaxies_with_numbers(matrix)
         total = compute_sum(num_matrix)
         self.assertEqual(total, 374)
 
-    def test_part1_sum(self):
+    def test_sample_sum_filler_10(self):
+        matrix = get_matrix(sample, 10)
+        num_matrix = replace_galaxies_with_numbers(matrix)
+        total = compute_sum(num_matrix)
+        self.assertEqual(total, 1030)
+
+    def test_sample_sum_filler_100(self):
+        matrix = get_matrix(sample, 100)
+        num_matrix = replace_galaxies_with_numbers(matrix)
+        total = compute_sum(num_matrix)
+        self.assertEqual(total, 8410)
+
+    def xtest_part1_sum(self):
         matrix = get_matrix(read_input("day11.txt"))
+        num_matrix = replace_galaxies_with_numbers(matrix)
+        total = compute_sum(num_matrix)
+        self.assertEqual(total, 374)
+
+    def test_part2_sum(self):
+        matrix = get_matrix(read_input("day11.txt"), 1000000)
         num_matrix = replace_galaxies_with_numbers(matrix)
         total = compute_sum(num_matrix)
         self.assertEqual(total, 374)
--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
 def read_input(file_name):
-    file = open(file_name, 'r')
+    file = open(file_name, "r")
     lines = file.readlines()
     file.close()
     values = []
     for line in lines:
         values.append(line.strip())
     return values
+
+
+def write_matrix_to_file(matrix, file_name):
+    with open(file_name, "w") as f:
+        for row in matrix:
+            f.write("".join(map(str, row)))
+            f.write("\n")
